874442c0cccdc28a1bf03bdbc21e2f475812ab61
Paul Drummond <paul.drummond@iode.co.uk>
2015-10-11 19:16:31 +0100

    Switching to timestamp based pagination.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 
-const MESSAGES_INCREMENT = 10;
+const MESSAGE_AGE_HOURS_INCREMENT = 3;
 
 var pageCounter = 0;
 
 if(Meteor.isClient) {
 
+  Template.registerHelper('formatDate', function (context, options) {
+  	if (context) {
+  		return moment(context).format('MMMM Do YYYY, h:mm:ss a');
+  	}
+  });
+
+
   Meteor.startup(function() {
-    Session.setDefault('messagesLimit', MESSAGES_INCREMENT);
+    Session.setDefault("messageAgeLimit", MESSAGE_AGE_HOURS_INCREMENT);
     Deps.autorun(function() {
       Meteor.subscribe('messages', {
-        limit: Session.get('messagesLimit'),
-        selectedMessageId: Session.get('selectedMessageId')
+        messageAgeLimit: Session.get('messageAgeLimit')
       });
     });
   });
@@ -21,74 +27,75 @@ if(Meteor.isClient) {
       messages.observeChanges({
         added: function(id, fields) {
           console.log('message inserted');
-          pageCounter++;
+          /*pageCounter++;
           if(pageCounter == MESSAGES_INCREMENT) {
-            console.log("BOOM - NEW MESSAGES ALL LOADED");
-            $("#message-list").scrollTop(($(".user-message").outerHeight() * MESSAGES_INCREMENT));
-          }
-        },
-        changed: function(id, fields) {
-          console.log('message updated');
-        },
-        removed: function() {
-          console.log('message removed');
-        }
-      });
-      return messages;
-    },
+          console.log("BOOM - NEW MESSAGES ALL LOADED");
+          $("#message-list").scrollTop(($(".user-message").outerHeight() * MESSAGES_INCREMENT));
+        }*/
+      },
+      changed: function(id, fields) {
+        console.log('message updated');
+      },
+      removed: function() {
+        console.log('message removed');
+      }
+    });
+    return messages;
+  },
 
-    moreResults: function() {
-      return !(Messages.find().count() < Session.get('messagesLimit'));
+  moreResults: function() {
+    //return !(Messages.find().count() < Session.get('messagesLimit'));
+    return true;
+  }
+
+});
+
+//message 100: eCuPav8Nfgp5jfwBN
+
+Template.app.onRendered(function() {
+  setTimeout(function() {
+    var selectedMessageId = Session.get('selectedMessageId');
+    if(selectedMessageId) {
+      /*$('#message-list').animate({
+      scrollTop: $("." + selectedMessageId).offset().top
+    }, 1000);*/
+    $(".user-message[data-id='" + selectedMessageId + "']").addClass("selected");
+  } else {
+    $("#message-list").scrollTop($("#message-list").height());
+  }
+  //$("#message-list").scroll(showMoreVisible);
+}, 2000);
+});
+
+function showMoreVisible() {
+  console.log('xxx');
+  var theshold, target = $("#showMoreResults");
+  if(!target.length) return;
+
+  threshold = $(window).scrollTop() + $(window).height() - target.height();
+  console.log("message list scrollTop:" + $("#message-list").scrollTop());
+  console.log("threshold:" + threshold);
+  console.log("target offset top: " + target.offset().top);
+  console.log("user message height: " + $(".user-message").outerHeight())
+
+  //if (target.offset().top == threshold) {
+  if($("#message-list").scrollTop() == 0) {
+    if (!target.attr("data-visible") == false) {
+      target.attr("data-visible", true);
     }
+    //$(".user-message:first").addClass('anchor-message');
+    Meteor.setTimeout(function() {
+      pageCounter = 0;
+      Session.set("messageAgeLimit", Session.get("messageAgeLimit") + MESSAGE_AGE_HOURS_INCREMENT);
+      //$(window).scrollTo(".anchor-message");
+    }, 5);
 
-  });
-
-  //message 100: eCuPav8Nfgp5jfwBN
-
-  Template.app.onRendered(function() {
-    setTimeout(function() {
-      var selectedMessageId = Session.get('selectedMessageId');
-      if(selectedMessageId) {
-        /*$('#message-list').animate({
-          scrollTop: $("." + selectedMessageId).offset().top
-        }, 1000);*/
-        $(".user-message[data-id='" + selectedMessageId + "']").addClass("selected");
-      } else {
-        $("#message-list").scrollTop($("#message-list").height());
-      }
-      $("#message-list").scroll(showMoreVisible);
-    }, 2000);
-  });
-
-  function showMoreVisible() {
-    console.log('xxx');
-    var theshold, target = $("#showMoreResults");
-    if(!target.length) return;
-
-    threshold = $(window).scrollTop() + $(window).height() - target.height();
-    console.log("message list scrollTop:" + $("#message-list").scrollTop());
-    console.log("threshold:" + threshold);
-    console.log("target offset top: " + target.offset().top);
-    console.log("user message height: " + $(".user-message").outerHeight())
-
-    //if (target.offset().top == threshold) {
-    if($("#message-list").scrollTop() == 0) {
-      if (!target.attr("data-visible") == false) {
-        target.attr("data-visible", true);
-      }
-      //$(".user-message:first").addClass('anchor-message');
-      Meteor.setTimeout(function() {
-        pageCounter = 0;
-        Session.set("messagesLimit", Session.get("messagesLimit") + MESSAGES_INCREMENT);
-        //$(window).scrollTo(".anchor-message");
-      }, 5);
-
-    } else {
-      if (target.attr("data-visible")) {
-        target.attr("data-visible", false);
-      }
+  } else {
+    if (target.attr("data-visible")) {
+      target.attr("data-visible", false);
     }
   }
+}
 }
 
 Messages = new Meteor.Collection('messages');
@@ -97,31 +104,28 @@ if(Meteor.isServer) {
 
   /*Meteor.startup(function() {
     Messages.remove({});
-    for(var i=1; i<=200; i++) {
+    for(var i=200; i>=1; i--) {
       Messages.insert({
         title: 'Message ' + i,
-        timestamp: new Date()
+        timestamp: moment().subtract({hours: i}).toDate().getTime()
       });
     }
   });*/
 
   Meteor.publish("messages", function(opts) {
-    if(opts && opts.selectedMessageId) {
-      console.log("selectedMessageId: " + opts.selectedMessageId);
-      var selectedMessage = Messages.findOne(opts.selectedMessageId);
-      filter = {timestamp: {$gte: selectedMessage.timestamp}};
-      options = {limit: opts.limit, sort: {timestamp: 1}};
-    } else {
-      filter = {}
-      options = {limit: opts.limit, sort: {timestamp: -1}};
-    }
-    return Messages.find(filter, options);
+    console.log("xxx: " + opts.messageAgeLimit);
+    var messageAge = moment(new Date()).subtract({hours: 12});//opts.messageAgeLimit});
+    console.log("messageAge: " + messageAge.format('MMMM Do YYYY, h:mm:ss a'));
+    var messageAgeTimestamp = messageAge.toDate().getTime();
+    //console.log("messageAgeTimestamp: " + messageAgeTimestamp);
+    return Messages.find({timestamp: {$gte: messageAgeTimestamp}}, {sort: {timestamp: -1}});
+    //return Messages.find({}, {sort: {timestamp: -1}});
   });
 }
 
 FlowRouter.route('/messages/:messageId', {
   action: function(params, queryParams) {
     console.log("MESSAGE ID IS " + params.messageId);
-    Session.set('selectedMessageId', params.messageId);
+    Session.set('startMessageId', params.messageId);
   }
 });
