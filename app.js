127eb8cd8ae8498a92bfce0418683fdd72076259
Paul Drummond <paul.drummond@iode.co.uk>
2015-10-13 16:36:51 +0100

    fixes for scrolling of message history

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 
-const MESSAGE_AGE_HOURS_INCREMENT = 10;
+OpenLoops = {};
+
+const MESSAGE_AGE_HOURS_INCREMENT = 24;
 
 var msgCountBeforePaging = 0;
 
 if(Meteor.isClient) {
 
+	OpenLoops.scrollToBottomOfMessages = function() {
+		$("#message-list").scrollTop($("#message-list")[0].scrollHeight);
+	}
+
 	Template.registerHelper('formatDate', function (context, options) {
 		if (context) {
 			return moment(context).format('MMMM Do YYYY, h:mm:ss a');
+		}
+	});
+
+	Template.registerHelper('currentItemTitle', function (context, options) {
+		var currentItemId = Session.get("currentItemId");
+		if(currentItemId) {
+			var currentItem = Items.findOne(currentItemId);
+			if(currentItem) {
+				return currentItem.title;
+			} else {
+				return "";
+			}
+		} else {
+			return "";
 		}
 	});
 
@@ -37,9 +57,10 @@ if(Meteor.isClient) {
 				Items.insert({
 					title: title,
 					description: description,
-					timestamp: new Date().getTime()
+					createdAt: new Date().getTime(),
+					createdBy: Meteor.user().username,
 				});
-				Session.set('currentPage', 'feedPage');
+				FlowRouter.go("/");
 			}
 		}
 	});
@@ -58,10 +79,13 @@ if(Meteor.isClient) {
 					if(inputVal.length > 0) {
 						Messages.insert({
 							title: inputVal,
-							timestamp: new Date().getTime(),
+							createdAt: new Date().getTime(),
+							createdBy: Meteor.user().username,
 							itemId: Session.get('currentItemId')
 						});
 						$("#message-box").val('');
+						OpenLoops.scrollToBottomOfMessages();
+
 					}
 				}
 			}
@@ -76,7 +100,7 @@ if(Meteor.isClient) {
 
 	Template.feed.helpers({
 		messages: function() {
-			return Messages.find({}, {sort: {timestamp: 1}});
+			return Messages.find({}, {sort: {createdAt: 1}});
 		}
 	});
 
@@ -87,11 +111,12 @@ if(Meteor.isClient) {
 	});
 
 	Template.app.onRendered(function() {
-		$("#message-list").scrollTop($("#message-list").height());
+		OpenLoops.scrollToBottomOfMessages();
 		$("#message-list").scroll(showMoreVisible);
 	});
 
 	function showMoreVisible() {
+		console.log("message-list scrollTop: " + $("#message-list").scrollTop());
 		if($("#message-list").scrollTop() == 0) {
 			msgCountBeforePaging = Messages.find().count();
 			Session.set("messageAgeLimit", Session.get("messageAgeLimit") + MESSAGE_AGE_HOURS_INCREMENT);
@@ -117,14 +142,14 @@ if(Meteor.isServer) {
 		var messageAgeTimestamp = messageAge.toDate().getTime();
 		return Messages.find({
 			itemId: opts.itemId,
-			timestamp: {$gte: messageAgeTimestamp}
+			createdAt: {$gte: messageAgeTimestamp}
 		}, {
-			sort: {timestamp: -1}
+			sort: {createdAt: -1}
 		});
 	});
 
 	Meteor.startup(function() {
-		//loadSampleData();
+		loadSampleData();
 	});
 
 	function loadSampleData() {
@@ -132,14 +157,16 @@ if(Meteor.isServer) {
 		var itemOneId = Items.insert({
 			title: 'Item One',
 			description: 'Item one description',
-			timestamp: new Date().getTime()
+			createdAt: new Date().getTime(),
+			createdBy: 'loopy'
 		});
 		console.log("ITEM ONE ID: " + itemOneId);
 		Messages.remove({});
 		for(var id=200, hour = 1; id>=1; id--, hour++) {
 			Messages.insert({
 				title: 'Message ' + id,
-				timestamp: moment().subtract({hours: hour}).toDate().getTime(),
+				createdBy: 'loopy',
+				createdAt: moment().subtract({hours: hour}).toDate().getTime(),
 				itemId: itemOneId
 			});
 		}
