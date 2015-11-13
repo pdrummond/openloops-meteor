d64ff3a6a6d83b7fadaf7ef721961c455daff99f
Paul Drummond <paul.drummond@iode.co.uk>
2015-11-13 13:16:08 +0000

    Added recount board/item messages dev-admin functions

--- dev-admin.js
+++ dev-admin.js
@@ -9,6 +9,28 @@ if(Meteor.isClient) {
 					$("#status").text("Added Default Project sucessfully. " + results.numBoards + " orphan boards added.");
 				}
 			});
+		},
+
+		'click #recount-board-messages': function() {
+			$("#status").text("Recount Board Messages: working...");
+			Meteor.call('recountBoardMessages', function(err, results) {
+				if(err) {
+					$("#status").text("Error recounting board messages: " + err.reason);
+				} else {
+					$("#status").text("Recounted board messages successfully.");
+				}
+			});
+		},
+
+		'click #recount-item-messages': function() {
+			$("#status").text("Recount Item Messages: working...");
+			Meteor.call('recountItemMessages', function(err, results) {
+				if(err) {
+					$("#status").text("Error recounting item messages: " + err.reason);
+				} else {
+					$("#status").text("Recounted item messages successfully.");
+				}
+			});
 		}
 	});
 }
@@ -34,6 +56,30 @@ if(Meteor.isServer) {
 			};
 			console.log("Added to default board: " + JSON.stringify(results, null, 4));
 			return results;
+		},
+
+		recountBoardMessages: function() {
+			console.log("> recountBoardMessages");
+			Boards.find().forEach(function(board) {
+				console.log("-- BEFORE num messages for board '" + board.title + "': " + board.numMessages);
+				var numMessages = ServerMessages.find({boardId: board._id}).count();
+				Boards.update(board._id, {$set: {numMessages: numMessages}});
+				board = Boards.findOne(board._id);
+				console.log("-- AFTER num messages for board '" + board.title + "': " + board.numMessages);
+			});
+			console.log("< recountBoardMessages");
+		},
+
+		recountItemMessages: function() {
+			console.log("> recountItemMessages");
+			Items.find({}).forEach(function(item) {
+				console.log("-- BEFORE num messages for item '" + item.title + "': " + item.numMessages);
+				var numMessages = ServerMessages.find({itemId: item._id}).count();
+				Items.update(item._id, {$set: {numMessages: numMessages}});
+				item = Items.findOne(item._id);
+				console.log("-- AFTER num messages for item '" + item.title + "': " + item.numMessages);
+			});
+			console.log("< recountItemMessages");
 		}
 	})
 }
