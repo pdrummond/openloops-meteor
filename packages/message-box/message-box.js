c373deedb819a5f65e412ff027888cdd44d9554e
Paul Drummond <paul.drummond@iode.co.uk>
2015-11-18 16:27:04 +0000

    moved message box to separate package and updated routes so auth is done at template level, not in routes

---
+++ packages/message-box/message-box.js
@@ -0,0 +1,31 @@
+if(Meteor.isClient) {
+	
+	Template.messageBox.events({
+		'keypress #messageBox': function(e) {
+			var inputVal = $('#messageBox').val();
+			var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+			if(charCode == 13 && (inputVal == null || inputVal.length == 0)) {
+				e.preventDefault();
+				e.stopPropagation();
+			} else {
+				if (charCode == 13 && e.shiftKey == false) {
+					e.preventDefault();
+					e.stopPropagation();
+					if(inputVal.length > 0) {
+						var newMessage = OpenLoops.insertClientMessage({text:inputVal});
+						$("#messageBox").val('');
+						Meteor.call('saveMessage', newMessage, function(err, result) {
+							if(err) {
+								alert("error sending message");
+							} else {
+								Ols.HistoryManager.scrollBottom();
+								Streamy.broadcast('sendMessage', newMessage);
+							}
+						});
+					}
+				}
+			}
+		}
+	});
+
+}
